d2468d90f81527284540a997a256849cb1221295
Daniel Schloms <daniel.schloms@gmail.com>
2025-06-03 12:52:43 +0000

    Update run script

--- run-target.sh
+++ run-target.sh
@@ -4,13 +4,22 @@ TARGET_GROUP="custom"
 TARGET_SW="vector_add"
 TARGET_ARCH="Vicuna"
 
+if [ "$#" -eq 1 ]; then
+    TARGET_SW=$1
+fi
+
+echo "Running $TARGET_SW."
+
 OUT_DIR="$WS_PATH/gen_perfsim/out"
-PARAMS="-tp=$OUT_DIR -ta=$OUT_DIR"
+# rm $OUT_DIR/VICUNA*
+PARAMS="-tp=$OUT_DIR" # -ta=$OUT_DIR"
 
 cd $WS_PATH/gen_perfsim
-./scripts/run.sh $TARGET_GROUP $TARGET_SW $TARGET_ARCH $PARAMS
 
 VICUNA_DIR="$WS_PATH/vicuna2_tinyml_benchmarking"
 TRACE_DIR="$VICUNA_DIR/trace_comparison"
+
+./scripts/run.sh $TARGET_GROUP $TARGET_SW $TARGET_ARCH $PARAMS > $TRACE_DIR/etiss/etiss_output.txt
+sed '/^0x/!d' $TRACE_DIR/etiss/etiss_output.txt > $TRACE_DIR/etiss/etiss_asm.txt
 
 cat $OUT_DIR/VICUNA_timing_* > $TRACE_DIR/etiss/etiss_timing.csv
