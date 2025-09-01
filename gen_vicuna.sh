3b9b10c6ec03aabbead86618c51900b4ccdeb148
Daniel Schloms <daniel.schloms@gmail.com>
2025-09-01 10:49:40 +0000

    Update scripts

--- gen_vicuna.sh
+++ gen_vicuna.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
 
 cd $WS_PATH/gen_perfsim
-VICUNA_CORE_PERF_DSL="$WS_PATH/gen_perfsim/code_gen/descriptions/core_perf_dsl/Vicuna.corePerfDsl"
+PERF_MODEL_DIR="$WS_PATH/gen_perfsim/code_gen/descriptions/core_perf_dsl"
+VICUNA_PERF_MODEL_IN="$PERF_MODEL_DIR/Vicuna.corePerfDsl.in"
+if [[ "$#" -eq 1 ]]; then
+  echo $1
+  VICUNA_PERF_MODEL_IN=$1
+fi
+VICUNA_CORE_PERF_DSL="$PERF_MODEL_DIR/Vicuna.corePerfDsl"
+FIFO_SCRIPT="$PERF_MODEL_DIR/insert_fifos.py"
 
+$FIFO_SCRIPT $VICUNA_PERF_MODEL_IN $VICUNA_CORE_PERF_DSL
 ./scripts/code_gen.sh $VICUNA_CORE_PERF_DSL
--- run-target.sh
+++ run-target.sh
@@ -1,30 +1,50 @@
 #!/usr/bin/bash
 
 TARGET_GROUP="custom"
-TARGET_ARCH="Vicuna"
 
-if [ "$#" -ne 4 ]; then
-    echo "run-target.sh <arch> <vlen> <vlane_width> <target>"
+
+if [ "$#" -lt 4 ] || [ "$#" -gt 5 ]; then
+    echo "run-target.sh <arch> <vlen> <vlane_width> <target> [--gdb]"
 fi
 
 GCC_ARCH=$1
 VLEN=$2
 VLANE_WIDTH=$3
 TARGET_SW=$4
+GDB_FLAG=""
+TARGET_ARCH="Vicuna_zvl${VLEN}b"
+# TARGET_ARCH="Vicuna"
 
+if [ "$5" = "--gdb" ]; then
+    GDB_FLAG="-gdbh"
+fi
+
+SCALAR_ARCH_INT="rv32im_zicsr"
+SCALAR_ARCH_FLOAT="rv32imf_zicsr"
 ZVL_STRING="zvl${VLEN}b"
-TARGET_SW_EXPLICIT=$GCC_ARCH/$ZVL_STRING/bin/$TARGET_SW
+VLANE_STRING="vlane${VLANE_WIDTH}"
+
 
 echo "Running $TARGET_SW build with ${GCC_ARCH}_$ZVL_STRING and VLANE_WIDTH ${VLANE_WIDTH}"
 
-OUT_DIR="$WS_PATH/gen_perfsim/out/$GCC_ARCH/$ZVL_STRING"
+ARCH_SUBPATH="$GCC_ARCH/$ZVL_STRING/$VLANE_STRING"
+TARGET_SW_EXPLICIT=$GCC_ARCH/$ZVL_STRING/bin/$TARGET_SW
+if [ "$ARCH" = "$SCALAR_ARCH_INT" ] || [ "$ARCH" = "$SCALAR_ARCH_FLOAT" ]; then
+    ARCH_SUBPATH=$GCC_ARCH
+    TARGET_SW_EXPLICIT=$GCC_ARCH/bin/$TARGET_SW
+fi
+
+OUT_DIR="$WS_PATH/gen_perfsim/out/$ARCH_SUBPATH/$TARGET_SW"
+DYN_PATH="$WS_PATH/gen_perfsim/etiss-perf-sim/simulator/dyn_inis/$ARCH_SUBPATH/$TARGET_SW"
 mkdir -p $OUT_DIR
-rm $OUT_DIR/VICUNA*
-PARAMS="-tp=$OUT_DIR" # -ta=$OUT_DIR"
+rm -f $OUT_DIR/VICUNA*
+rm -f $OUT_DIR/asm_trace*
+PARAMS="-tp=$OUT_DIR --dyn_path=$DYN_PATH -ta=$OUT_DIR $GDB_FLAG"
+# PARAMS="-tp=$OUT_DIR"
 
 COMPARISON_DIR="$WS_PATH/rvv_testing/comparison"
 VICUNA_DIR="$WS_PATH/vicuna2_tinyml_benchmarking"
-TRACE_DIR="$COMPARISON_DIR/etiss/$GCC_ARCH/$ZVL_STRING/vlane$VLANE_WIDTH"
+TRACE_DIR="$COMPARISON_DIR/etiss/$ARCH_SUBPATH"
 mkdir -p $TRACE_DIR
 
 export VLEN=$VLEN
@@ -32,7 +52,10 @@ export VLANE_WIDTH=$VLANE_WIDTH
 
 cd $WS_PATH/gen_perfsim
 
-./scripts/run.sh $TARGET_GROUP $TARGET_SW_EXPLICIT $TARGET_ARCH $PARAMS > $TRACE_DIR/${TARGET_SW}_output.txt
-sed '/^0x/!d' $TRACE_DIR/${TARGET_SW}_output.txt > $TRACE_DIR/${TARGET_SW}_trace.txt
+./scripts/run.sh $TARGET_GROUP $TARGET_SW_EXPLICIT $TARGET_ARCH $PARAMS
+TRACE_FILE=$TRACE_DIR/${TARGET_SW}_trace.txt
+# > $TRACE_DIR/${TARGET_SW}_output.txt
 
-cat $OUT_DIR/VICUNA_timing_* > $TRACE_DIR/${TARGET_SW}_timing.csv
+cat $OUT_DIR/VICUNA*_timing_* > $TRACE_DIR/${TARGET_SW}_timing.csv
+cat $OUT_DIR/asm_trace_* > $TRACE_DIR/${TARGET_SW}_trace.txt
+sed -i '/^0x/!d' $TRACE_FILE
