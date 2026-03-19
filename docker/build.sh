cad49faae3727b59cef9dc6e7e323f5b29ec1992
Daniel Schloms <daniel.schloms@gmail.com>
2026-03-19 10:42:50 +0100

    Enable build script to be called from anywhere

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+DOCKER_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+cd $DOCKER_DIR || exit
 echo "Building Ubuntu 24.04 image for performance estimation"
 docker build -t perfsim-img --build-arg USERNAME=$USER --build-arg USER_UID=$UID --build-arg USER_GID=$(id -g $USER) .
