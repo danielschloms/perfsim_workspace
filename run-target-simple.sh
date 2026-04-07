94fcf6ef0ae448193924617187439096b9768ce6
Daniel Schloms <daniel.schloms@gmail.com>
2026-04-07 17:08:38 +0200

    Remove usage of WS_PATH in simple runner script

--- run-target-simple.sh
+++ run-target-simple.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+PERFSIM_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+PROJECT_ROOT_DIR="$(dirname "$SCRIPTS_DIR")"
+
 VLEN="128"
 VLANE_WIDTH="32"
 TARGET_SW=""
@@ -9,49 +12,49 @@ PROFILER=""
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    --arch)
-      ARCH="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    --vlen)
-      VLEN="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    --target)
-      TARGET_SW="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    --ca)
-      ARCH_EXT="_ca"
-      shift # past argument
-      ;;
-    --perf)
-      USE_PERF="true"
-      shift # past argument
-      ;;
-    --profile)
-      echo "Using profiler"
-      PROFILER="valgrind --tool=callgrind "
-      shift # past argument
-      ;;
-    -*|--*)
-      >&2 echo "Unknown option $1"
-      exit 1
-      ;;
-    *)
-      POSITIONAL_ARGS+=("$1") # save positional arg
-      shift # past argument
-      ;;
+  --arch)
+    ARCH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  --vlen)
+    VLEN="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  --target)
+    TARGET_SW="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  --ca)
+    ARCH_EXT="_ca"
+    shift # past argument
+    ;;
+  --perf)
+    USE_PERF="true"
+    shift # past argument
+    ;;
+  --profile)
+    echo "Using profiler"
+    PROFILER="valgrind --tool=callgrind "
+    shift # past argument
+    ;;
+  -* | --*)
+    >&2 echo "Unknown option $1"
+    exit 1
+    ;;
+  *)
+    POSITIONAL_ARGS+=("$1") # save positional arg
+    shift                   # past argument
+    ;;
   esac
 done
 
 ZVL_STRING="zvl${VLEN}b"
 TARGET_ARCH="Vicuna_$ZVL_STRING"
 
-SIM_DIR="$WS_PATH/gen_perfsim/etiss-perf-sim/simulator"
+SIM_DIR="$PERFSIM_DIR/etiss-perf-sim/simulator"
 DYN_DIR="$SIM_DIR/dyn_inis/rv32im_zve32x/${ZVL_STRING}/vlane32/${TARGET_SW}"
 
 COMMON_INI="$SIM_DIR/ini/common.ini"
@@ -72,4 +75,3 @@ else
   echo "Perf off"
   time $ETISS_EXE -i$COMMON_INI -i$ADDR_INI -i$ARCH_INI -i$BIN_INI
 fi
-
